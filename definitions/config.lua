eb3929c8f0bbc1737a6de2ed3cf149448e1b201e
Kuba Mazurek <zakaprov@gmail.com>
2022-10-16 17:48:34 +0200

    WIP: personal keymap

--- definitions/config.lua
+++ definitions/config.lua
@@ -8,7 +8,7 @@ return {
   show_feedback_window = true,
   -- should operators in visual modes reset the selection or have it persist?
   persist_visual_timeline_selection = true,
-  persist_visual_track_selection = false,
+  persist_visual_track_selection = true,
   -- allow timeline movement when in visual track mode?
   allow_visual_track_timeline_movement = true,
   -- options in decreasing verbosity: [trace debug info warn user error fatal]
